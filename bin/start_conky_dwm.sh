0c722ed522371bea4433dab0d6faaaab33db8bac
Janis Pagel <janis.pagel@ims.uni-stuttgart.de>
2018-07-15 11:39:28 +0200

    Only start conky if needed

---
+++ bin/start_conky_dwm.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+conky -c ~/.conky/.conkyrc_dwm
+conky -c ~/.conky/.conkyrc_tmux
+conky -c ~/.conky/.conkyrc_vim
--- dotfiles.sh
+++ dotfiles.sh
@@ -32,6 +32,9 @@ rsync -ac --progress $GIT_REPO/vimrc ~/.vimrc
 
 rsync -ac --progress $GIT_REPO/scripts/tmux_pane_current_path.sh ~/scripts/tmux_pane_current_path.sh
 
+rsync -ac --progress $GIT_REPO/bin/start_conky_dwm.sh ~/bin/start_conky_dwm.sh
+rsync -ac --progress $GIT_REPO/bin/start_conky_i3.sh ~/bin/start_conky_i3.sh
+
 rsync -ac --progress ~/suckless/dwm-6.1/config.h $GIT_REPO/suckless/dwm/config.h
 rsync -ac --progress ~/suckless/st-0.8.1/config.h $GIT_REPO/suckless/st/config.h
 rsync -ac --progress ~/suckless/st-0.8.1/st.info $GIT_REPO/suckless/st/st.info
