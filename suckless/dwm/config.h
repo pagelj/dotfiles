089abdd23aa496c97f228ff284dd5031d2b2e68e
Janis Pagel <janis.pagel@ims.uni-stuttgart.de>
2018-08-07 15:53:44 +0300

    Hide bar by default in dwm

--- suckless/dwm/config.h
+++ suckless/dwm/config.h
@@ -13,7 +13,7 @@ static const char selbgcolor[]      = "#005577";
 static const char selfgcolor[]      = "#eeeeee";
 static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
-static const int showbar            = 1;        /* 0 means no bar */
+static const int showbar            = 0;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 
 /* tagging */
