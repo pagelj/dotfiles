daf9c0401a4b46e72f56bde4955f8c4b56cf1744
Janis Pagel <janis.pagel@ims.uni-stuttgart.de>
2019-03-18 23:56:38 +0100

    Display also public IP and VPN status

--- bin/cowsay_ip.sh
+++ bin/cowsay_ip.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-st -e watch -n 0.1 -t 'for i in `ip r`; do echo $i; done | grep -A 1 src | tail -1 | cowsay'
+st -e watch -n 0.1 -t 'for i in `ip r`; do echo $i; done | grep -A 1 src | tail -1 | cowsay && dig +short myip.opendns.com @resolver1.opendns.com | cowthink; if [ -d "/proc/sys/net/ipv4/conf/tun0" ]; then cowsay -p VPN; fi'
