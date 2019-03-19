c9ec7caa11df384d85e00e501e5de7c176c25c0d
Janis Pagel <janis.pagel@ims.uni-stuttgart.de>
2019-03-19 15:54:21 +0100

    Remove retrieval of public IP address

--- bin/dmenu_statusbar.sh
+++ bin/dmenu_statusbar.sh
@@ -8,7 +8,6 @@ fi
 LOCALTIME=$(date +%Z%Y-%m-%dW%V%a%H:%M:%S)
 UTC=$(TZ=UTC date +%Z%Y-%m-%dW%V%a%H:%M:%S)
 IP="$(for i in `ip r`; do echo $i; done | grep -A 1 src | tail -n1)"
-PUBIP="$(dig +short myip.opendns.com @resolver1.opendns.com)"
 if [ -d "/proc/sys/net/ipv4/conf/tun0" ]
 then
 	VPN="yes"
@@ -18,4 +17,4 @@ fi
 TEMP="$(($(cat /sys/class/thermal/thermal_zone0/temp) / 1000))°C"
 BAT="$(acpi -b | awk '{ print $4 " " }' | tr -d ',' | tr -d ' ')"
 
-echo -e "time=$LOCALTIME\nvol=$VOL\nip=$IP\npubip=$PUBIP\nvpn=$VPN\ntemp=$TEMP\nbat=$BAT\nutc=$UTC" | dmenu -fn "Sans:size=11"
+echo -e "time=$LOCALTIME\nvol=$VOL\nip=$IP\nvpn=$VPN\ntemp=$TEMP\nbat=$BAT\nutc=$UTC" | dmenu -fn "Sans:size=11"
