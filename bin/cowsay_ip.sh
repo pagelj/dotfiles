45593860bd1f013a5982bfe280dfe0b25eadfe2b
Janis Pagel <janis.pagel@ims.uni-stuttgart.de>
2019-06-19 20:30:09 +0200

    Use nordvpn binary for VPN status message

--- bin/cowsay_ip.sh
+++ bin/cowsay_ip.sh
@@ -1,3 +1,3 @@
-#!/bin/sh
+#!/bin/dash
 
-st -e watch -n 0.1 -t 'for i in `ip r`; do echo $i; done | grep -A 1 src | tail -1 | cowsay; if [ -d "/proc/sys/net/ipv4/conf/tun0" ]; then cowsay -b VPN; else cowsay -p no VPN; fi'
+st -e watch -n 0.1 -t 'for i in `ip r`; do echo $i; done | grep -A 1 src | tail -1 | cowsay; if [ -d "/proc/sys/net/ipv4/conf/tun0" ]; then nordvpn status | cowsay -b; else cowsay -p no VPN; fi'
