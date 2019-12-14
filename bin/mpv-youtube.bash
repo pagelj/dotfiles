4866f7501bc37a225a9d7082bc5048f9f1a99ccf
Janis Pagel <janis.pagel@ims.uni-stuttgart.de>
2019-12-15 00:25:12 +0100

    Add script for launching bookmarks into mpv

---
+++ bin/mpv-youtube.bash
@@ -0,0 +1,14 @@
+#!/bin/env bash
+
+BOOKMARKS="$HOME/.bookmarks.html"
+TMPFILE=$HOME/.tmp/mpv-youtube.last
+LASTENTRY=`< $TMPFILE`
+
+ENTRY=$(cat <(printf 'Last: %s\n\n' "$LASTENTRY") <(paste -d' ' <(pup -f $BOOKMARKS "dt a attr{adddate}") <(pup -f $BOOKMARKS "dt a text{}") <(pup -f $BOOKMARKS "dt a attr{href}") | egrep "https?://(www\.)?youtube\.(com|de)" | sort -r | cut -d' ' -f2-) | dmenu -p "YouTube Bookmarks:" -i -l 30)
+if [ -z "$ENTRY" ]; then
+	exit 0
+else
+	echo $ENTRY > $TMPFILE
+fi
+LINK=$(echo $ENTRY | tr ' ' '\n' | tail -1)
+mpv --x11-name mpv-youtube $LINK
