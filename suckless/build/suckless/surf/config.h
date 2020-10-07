bdceedd040ff548612ee283fbf71834714c2ead4
Janis Pagel <janis.pagel@ims.uni-stuttgart.de>
2020-10-08 01:57:15 +0200

    Change config file location for most programs

--- suckless/build/suckless/surf/config.h
+++ suckless/build/suckless/surf/config.h
@@ -1,10 +1,10 @@
 /* modifier 0 means no modifier */
 static int surfuseragent    = 1;  /* Append Surf version to default WebKit user agent */
 static char *fulluseragent  = ""; /* Or override the whole user agent string */
-static char *scriptfile     = "~/.surf/script.js";
-static char *styledir       = "~/.surf/styles/";
-static char *cachedir       = "~/.surf/cache/";
-static char *cookiefile     = "~/.surf/cookies.txt";
+static char *scriptfile     = "~/.local/share/surf/script.js";
+static char *styledir       = "~/.local/share/surf/styles/";
+static char *cachedir       = "~/.cache/surf/";
+static char *cookiefile     = "~/.local/share/surf/cookies.txt";
 
 /* Webkit default features */
 static Parameter defconfig[ParameterLast] = {
