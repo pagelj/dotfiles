a6f7daf9d21a8ddceba93664c75875ec89010817
Janis Pagel <janis.pagel@ims.uni-stuttgart.de>
2020-12-19 18:18:12 +0100

    Fix path

--- scripts/.local/scripts/convert_bookmarks_html2text.sh
+++ scripts/.local/scripts/convert_bookmarks_html2text.sh
@@ -1,6 +1,8 @@
 #!/bin/env bash
 
-BOOKMARKS_HTML="$HOME/.bookmarks.html"
-BOOKMARKS_TXT="$HOME/.bookmarks"
+# Convert Firefox-style html bookmarks to plain text
 
-paste -d' ' <(pup -f $BOOKMARKS_HTML "dt a attr{adddate}") <(pup -f $BOOKMARKS_HTML "dt a attr{href}") <(pup -f $BOOKMARKS_HTML "dt a text{}") | sort | cut -d' ' -f 2- > $BOOKMARKS_TXT
+BOOKMARKS_HTML="$XDG_DATA_HOME/bookmarks/bookmarks.html"
+BOOKMARKS_TXT="$XDG_DATA_HOME/bookmarks/bookmarks"
+
+paste -d' ' <(pup -f "$BOOKMARKS_HTML" "dt a attr{adddate}") <(pup -f "$BOOKMARKS_HTML" "dt a attr{href}") <(pup -f "$BOOKMARKS_HTML" "dt a text{}") | sort | cut -d' ' -f 2- > "$BOOKMARKS_TXT"
