a55a7b71586358c3e1bf6d330ab0baf45a14bc5c
Janis Pagel <janis.pagel@uni-koeln.de>
2022-07-11 16:16:32 +0200

    Create needed folders in install script

--- install.sh
+++ install.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env sh
 
+XDG_DATA_HOME="$HOME"/.local/share
+
 stow abook
 stow autostart
 stow bin
@@ -7,6 +9,9 @@ stow dunst
 stow emacs
 stow gnupg
 stow mime
+mkdir -p "$XDG_DATA_HOME"/mpd
+mkdir -p "$XDG_DATA_HOME"/mpd/playlists
+mkdir -p "$XDG_DATA_HOME"/mpd/database
 stow mpd
 stow ncmpcpp
 stow newsboat
@@ -22,4 +27,5 @@ stow tmux
 stow top
 stow xinit
 stow zathura
+mkdir -p "$XDG_DATA_HOME"/zsh
 stow zsh
