8c0a7e0081decffc18c6c6dbcb2c5a1dffd0c254
Janis Pagel <janis.pagel@uni-koeln.de>
2022-10-09 11:04:11 +0200

    Update tabbed to version 0.7

--- suckless/.local/build/suckless/tabbed/config.h
+++ suckless/.local/build/suckless/tabbed/config.h
@@ -7,6 +7,8 @@ static const char* normbgcolor  = "#222222";
 static const char* normfgcolor  = "#cccccc";
 static const char* selbgcolor   = "#555555";
 static const char* selfgcolor   = "#ffffff";
+static const char* urgbgcolor   = "#111111";
+static const char* urgfgcolor   = "#cc0000";
 */
 /* nord */
 static const char* normbgcolor  = "#4c566a";
