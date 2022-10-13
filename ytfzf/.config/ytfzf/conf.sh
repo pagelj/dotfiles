06cc52d45cc6680cdbe96f7791b4cf10893f2950
Janis Pagel <janis.pagel@uni-koeln.de>
2022-10-13 14:31:25 +0200

    Add vlc option

--- ytfzf/.config/ytfzf/conf.sh
+++ ytfzf/.config/ytfzf/conf.sh
@@ -1,2 +1,4 @@
 url_handler="mpv"
 url_handler_opts="--no-terminal --ontop --no-focus-on-open --no-border --force-window --x11-name=\"mpv-newsboat\""
+#url_handler="vlc"
+#url_handler_opts="--play-and-exit --preferred-resolution -1 --qt-minimal-view"
