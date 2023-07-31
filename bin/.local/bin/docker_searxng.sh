55ee0f631a23760d1282f0bcf79c7ab84e684366
Janis Pagel <janis.pagel@uni-koeln.de>
2023-07-31 23:39:42 +0200

    Add docker searxng start script

---
+++ bin/.local/bin/docker_searxng.sh
@@ -0,0 +1,8 @@
+export PORT=8080
+docker pull searxng/searxng
+docker run --rm \
+           -d -p ${PORT}:8080 \
+           -v "$HOME/.config/searxng:/etc/searxng" \
+           -e "BASE_URL=http://localhost:$PORT/" \
+           -e "INSTANCE_NAME=searxng" \
+           searxng/searxng
