cde30ab4cdc022ccbe3a5619e91baa0737fb7a50
Janis Pagel <janis.pagel@uni-koeln.de>
2023-08-08 04:27:40 +0200

    Add AppImage to AUR clean script

--- bin/.local/bin/aur-cleanup.sh
+++ bin/.local/bin/aur-cleanup.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env dash
 
-find "$HOME"/.local/build/aur -depth -mindepth 1 -maxdepth 2 \( -type f -name '*.zst' -o -name '*.zip' -o -name '*.gz' -o -name '*.tgz' -o -name '*.bz2' -o -name '*.deb' -o -name '*.asc' -o -name '*.rpm' -o -name '*.tar.xz' \) -delete
+find "$HOME"/.local/build/aur -depth -mindepth 1 -maxdepth 2 \( -type f -name '*.zst' -o -name '*.zip' -o -name '*.gz' -o -name '*.tgz' -o -name '*.bz2' -o -name '*.deb' -o -name '*.asc' -o -name '*.rpm' -o -name '*.tar.xz' -o -name '*.AppImage' \) -delete
